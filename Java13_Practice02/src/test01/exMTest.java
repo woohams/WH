f7f344356f5edcdc94c6d8d4dbf37195b7683037
woohams <50380276+woohams@users.noreply.github.com>
2019-08-05 23:41:47 +0900

    Add files via upload

---
+++ Java13_Practice02/src/test01/exMTest.java
@@ -0,0 +1,13 @@
+package test01;
+
+public class exMTest {
+
+	public static void main(String[] args) {
+		ex02 star = new ex02();
+		star.StarSquare();
+		star.StarDiamond();
+		
+		star.Fibonacci(13);
+	}
+
+}
